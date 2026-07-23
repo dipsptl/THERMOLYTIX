211a9d8aa43280997683bcfc63a7d011202353e1
DipalPatel <amspatel33@gmail.com>
2026-07-23 18:40:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,8 +123,8 @@ st.markdown(f"""
         color: #000000 !important;
         border: none !important;
         border-radius: 8px !important;
-        padding: 0.2rem 0.7rem !important;
-        font-weight: 800 !important; font-size: 1.4rem !important;
+        padding: 0.1rem 0.7rem !important;
+        font-weight: 800 !important; font-size: 1.6rem !important;
         box-shadow: none !important;
     }}
     .st-key-login_btn button:hover {{
