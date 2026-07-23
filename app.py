e07ce9ed9e9b32080fc5daaddbb65debe6d50379
DipalPatel <amspatel33@gmail.com>
2026-07-23 18:50:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,12 +123,12 @@ st.markdown(f"""
         color: #000000 !important;
         border: none !important;
         border-radius: 8px !important;
-        padding: 0.03rem 0.9rem !important;
-        font-weight: 800 !important; font-size: 2.6rem !important;
+        padding: 0.25rem 0.9rem !important;
+        font-weight: 800 !important; font-size: 1.15rem !important;
         box-shadow: none !important;
     }}
     .st-key-login_btn button p {{
-        font-size: 2.6rem !important;
+        font-size: 1.15rem !important;
         font-weight: 800 !important;
         line-height: 1.2 !important;
         margin: 0 !important;
